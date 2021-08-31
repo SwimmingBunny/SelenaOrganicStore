8504a46487608ef0ab7596e579166c65fa3e617f
nguyenthtran.dev@gmail.com <nguyengcd18556@fpt.edu.vn>
2021-08-31 19:23:02 +0700

    fix app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import "./style/style.scss";
 import Header from "./component/layout/Header";
 import Home from "./pages/Home/Home";
@@ -7,12 +9,9 @@ import Footer from "./component/layout/Footer";
 import Login from "./pages/form/formLogin/Login";
 import Register from "./pages/form/formRegister/Register";
 import ListItem from "./pages/shop/ShopListItem/ListItem";
-<<<<<<< HEAD
 import ProductDetail from "./pages/productDetails/ProductDetails";
 import Contact from "./pages/contact/Contact";
-=======
 import AboutUs from "./pages/aboutUs/AboutUs";
->>>>>>> 5eb6efd458f940c61d3f347fe47797786926a63c
 
 function App() {
   return (
@@ -34,16 +33,17 @@ function App() {
           <Route path={ROUTE.SHOPITEM} exact>
             <ListItem />
           </Route>
-<<<<<<< HEAD
-          <Route  path={ROUTE.SHOPDETAIL} exact>
+          <Route path={ROUTE.SHOPDETAIL} exact>
             <ProductDetail />
           </Route>
-          <Route  path={ROUTE.CONTACT} exact>
+          <Route path={ROUTE.CONTACT} exact>
             <Contact />
-=======
+          </Route>
+          <Route path={ROUTE.CART} exact>
+            <Cart/>
+          </Route>
           <Route path={ROUTE.ABOUTUS} exact>
             <AboutUs />
->>>>>>> 5eb6efd458f940c61d3f347fe47797786926a63c
           </Route>
         </Switch>
       </div>
